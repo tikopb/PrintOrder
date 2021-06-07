581c0957309a91425d30a7514fd28f8683bb9229
ict-marissa <marissanaini@gmail.com>
2021-06-07 15:40:01 +0700

    import dan list data

---
+++ app/src/main/java/com/example/laundry/DataHelper.java
@@ -0,0 +1,29 @@
+package com.example.laundry;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DataHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "laundry.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public DataHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        // TODO Auto-generated constructor stub
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // TODO Auto-generated method stub
+        String sql = "create table customers(nomor char primary key, nama text null, no_hp integer null, jk text null, alamat text null);";
+        Log.d("Data", "onCreate: " + sql);
+        db.execSQL(sql);
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
+        // TODO Auto-generated method stub
+    }
+}
